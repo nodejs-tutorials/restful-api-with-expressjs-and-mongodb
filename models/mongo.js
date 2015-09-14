b901e4a98879097440060854c4748ca48322997f
Nguyen Ngoc Duc <ducnn01@gmail.com>
2015-09-14 22:51:00 +0700

    first commit

---
+++ models/mongo.js
@@ -0,0 +1,11 @@
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/demoDb');
+// create instance of Schema
+var mongoSchema = mongoose.Schema;
+// create schema
+var userSchema = {
+    "userEmail"   : String,
+    "userPassword": String
+};
+// create model if not exists.
+module.exports = mongoose.model('userLogin', userSchema);
